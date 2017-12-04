fbc7c305b1b33cdb07e89ab15418b1e731d63146
Aimu UEMA <e175747@ie.u-ryukyu.ac.jp>
2017-12-05 08:26:31 +0900

    コード準備

--- prog2/Report4_1/src/main/jp/ac/uryukyu/ie/e175747/Main.java
+++ prog2/Report4_1/src/main/jp/ac/uryukyu/ie/e175747/Main.java
@@ -3,11 +3,11 @@ package jp.ac.uryukyu.ie.e175747;
 public class Main {
     public static void main(String[] args){
         String str = null;
-        try{
+        //try{
             System.out.println(str.length());
-        }catch(NullPointerException e){
-            System.out.println("NullPointerExceptionが報告されました");
-            System.out.println(e.getMessage());
-        }
+        //}catch(NullPointerException e){
+            //System.out.println("NullPointerExceptionが報告されました");
+            //System.out.println(e.getMessage());
+        //}
     }
 }
