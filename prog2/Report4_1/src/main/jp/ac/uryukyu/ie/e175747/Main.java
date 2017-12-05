09f1e42e14dc0e99755ed102228e378a1223b6ed
Aimu UEMA <e175747@ie.u-ryukyu.ac.jp>
2017-12-05 09:02:14 +0900

    double型変数

--- prog2/Report4_1/src/main/jp/ac/uryukyu/ie/e175747/Main.java
+++ prog2/Report4_1/src/main/jp/ac/uryukyu/ie/e175747/Main.java
@@ -2,12 +2,16 @@ package jp.ac.uryukyu.ie.e175747;
 
 public class Main {
     public static void main(String[] args){
-        String str = null;
+        String str ="3．14";
         try{
-            System.out.println(str.length());
-        }catch(NullPointerException e){
+
+            double value = Double.parseDouble(str);
+
+        }catch(Exception e){
+
             System.out.println("NullPointerExceptionが報告されました");
             System.out.println(e.getMessage());
+
         }
     }
 }
